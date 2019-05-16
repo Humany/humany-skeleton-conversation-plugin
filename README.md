b4792d838bfcb40283637155871cb9474885fea7
totte <theomuller@hotmail.com>
2019-05-16 13:02:26 +0200

    Add project

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # humany-skeleton-conversation-plugin
-Skeleton plugin using @humany/widget-conversation
+This skeleton sets up a plugin for Humany using the Conversation Platform.
+
+For documentation on how to use the platform: [`@humany/widget-conversation`](https://www.npmjs.com/package/@humany/widget-conversation).
+
+## Building the code
+1. Ensure NodeJS and Git is installed.
+2. Clone or fork this repository.
+3. From the root folder, execute the following command to install the dependencies:
+```
+npm install / yarn
+```
+4. From the root folder, execute one of the following commands to generate the bundle.
+```
+npm run build / yarn build
+```
+```
+npm run build-dev / yarn build-dev
+```
+5. To watch the plugin in action
+```
+npm run start / yarn start
+```
+
+This will launch your browser and redirect you to a simple server where you can see your plugin in action.
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (function () {
+  const config = {};
+
+  config.mode = 'development'
+
+  config.entry = [__dirname + '/src/polyfills.js', __dirname + '/src/index.js'];
+
+  config.output = {
+    filename: 'humany.js',
+    chunkFilename: 'humany-[name].js',
+    path: __dirname + '/dist',
+  };
+
+  config.plugins = [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/public/index.html',
+    }),
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1,
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ];
+
+  config.module = {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { modules: false }]],
+            plugins: [
+              'babel-plugin-syntax-dynamic-import',
+              'babel-plugin-transform-class-properties',
+              'babel-plugin-transform-async-to-generator',
+            ],
+          },
+        },
+      },
+    ],
+  };
+
+  return config;
+})();
